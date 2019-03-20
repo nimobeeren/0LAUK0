e7690b0d1bffc5be95f8b3cdc77ae0d18e6097bc
Nimo Beeren <nimobeeren@gmail.com>
2019-03-20 12:41:03 +0100

    Refactor pathfinding into separate script

--- Assets/Behaviours/TargetBehaviour.cs
+++ Assets/Behaviours/TargetBehaviour.cs
@@ -2,196 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
-{
-    // The GameObject that represents this node
-    public GameObject obj;
-
-    // All nodes that can be reached from this one
-    public List<Node> neighbors;
-
-    // The previous node in the path
-    public Node cameFrom = null;
-
-    // The cost of getting from the start to this node
-    public float gScore = Mathf.Infinity;
-
-    // The cost of getting from the start to the goal while passing by this node
-    // This value is partly known, partly heuristic
-    public float fScore = Mathf.Infinity;
-
-    // A shorthand for obj.transform.position
-    public Vector3 Position
-    {
-        get { return obj.transform.position; }
-        set { obj.transform.position = value; }
-    }
-
-    public Node(GameObject obj)
-    {
-        this.obj = obj;
-        neighbors = new List<Node>();
-    }
-
-    public static float Distance(Node v1, Node v2)
-    {
-        return Vector3.Distance(v1.Position, v2.Position);
-    }
-}
-
 public class TargetBehaviour : MonoBehaviour
 {
-	public GameObject destination;  // the final destination of the drone
-	public float speed;				// the (constant) speed at which the target moves to the destination
-	public float waypointMargin;	// the maximum distance at which a waypoint is considered 'reached'
+    public GameObject destination;       // the final destination of the drone
+    public float speed = 2;              // the (constant) speed at which the target moves to the destination
+    public float waypointMargin = 0.1f;  // the maximum distance at which a waypoint is considered 'reached'
 
-	private bool flying = false;	// whether the drone is currently flying
-	private Rigidbody rb;			// the target's rigidbody
-    private List<Node> navNodes;    // waypoints used for pathfinding
-    private Node nextNode;          // the node that the target is currently moving towards
-    private List<Node> path;		// the calculated path from the starting point to the destination
-
-    List<Node> ReconstructPath(Node current)
-    {
-        List<Node> path = new List<Node>();
-        path.Add(current);
-        while (current.cameFrom != null)
-        {
-            current = current.cameFrom;
-            path.Add(current);
-        }
-        return path;
-    }
-
-    List<Node> AStar(Node start, Node goal)
-    {
-        // The set of nodes already evaluated
-        List<Node> closedSet = new List<Node>();  // TODO: use HashSet
-
-        // The set of currently discovered nodes that are not evaluated yet
-        List<Node> openSet = new List<Node>();
-        openSet.Add(start);
-
-        // The cost of going from start to start is zero
-        start.gScore = 0;
-
-        // For the first node, the cost to get to the goal is estimated using the heuristic
-        start.fScore = Node.Distance(start, goal);
-
-        while (openSet.Count > 0)
-        {
-            // Find the node with lowest fScore
-            Node current = null;
-            float maxF = Mathf.Infinity;
-            foreach (Node v in openSet)
-            {
-                if (current == null || v.fScore < maxF)
-                {
-                    current = v;
-                    maxF = v.fScore;
-                }
-            }
-
-            if (current == goal)
-            {
-                return ReconstructPath(current);
-            }
-
-            openSet.Remove(current);
-            closedSet.Add(current);
-
-            foreach (Node neighbor in current.neighbors)
-            {
-                if (closedSet.Contains(neighbor)) continue;  // ignore neighbors that have already been evaluated
-
-                // The distance from start to a neighbor
-                float tentativeGScore = current.gScore + Node.Distance(current, neighbor);
-
-                if (!openSet.Contains(neighbor))  // discover a new node
-                {
-                    openSet.Add(neighbor);
-                }
-                else if (tentativeGScore >= neighbor.gScore)
-                {
-                    continue;
-                }
-
-                // This path is the best yet, record it!
-                neighbor.cameFrom = current;
-                neighbor.gScore = tentativeGScore;
-                neighbor.fScore = neighbor.gScore = Node.Distance(neighbor, goal);
-            }
-        }
-
-        // We failed to find a path from start to goal
-        return null;
-    }
+    private bool flying = false;         // whether the drone is currently flying
+    private Rigidbody rb;                // the target's rigidbody
+    private List<Vector3> path;          // sequence of waypoints to follow to get to the destination
 
     // Start is called before the first frame update
     void Start()
     {
-    	// Get our own Rigidbody component
-    	rb = GetComponent<Rigidbody>();
+        // Get our own Rigidbody component
+        rb = GetComponent<Rigidbody>();
 
-        // Get NavNodes
-        navNodes = new List<Node>();
-        GameObject[] nodeObjs = GameObject.FindGameObjectsWithTag("NavNode");
-        foreach (GameObject obj in nodeObjs)
+        // Find shortest path from current position to destination, using navNodes as waypoints
+        path = Pathfinding.GetShortestPath(gameObject, destination);
+
+        // Display the path
+        if (path == null || path.Count == 0)
         {
-            navNodes.Add(new Node(obj));
-        }
-
-        // Create nodes for itself and destination
-        Node start = new Node(gameObject);
-        Node goal = new Node(destination);
-        navNodes.Add(start);
-        navNodes.Add(goal);
-
-        // For each node, find its neighbors
-        // Two nodes are neighbors iff they have line of sight to eachother
-        foreach (Node v in navNodes)
-        {
-            foreach (Node u in navNodes)
-            {
-                if (v == u) continue;
-                Vector3 origin = v.Position;
-                Vector3 direction = u.Position - v.Position;
-                float dist = Node.Distance(v, u);
-                int layerMask = ~(1 << 9);  // we can see through objects in Debug layer (9)
-                if (!Physics.Raycast(origin, direction, dist, layerMask))
-                {
-                    // Nodes have line of sight to eachother
-                    v.neighbors.Add(u);
-                    u.neighbors.Add(v);
-                    Debug.DrawRay(origin, direction.normalized * dist, Color.green, Mathf.Infinity);
-                }
-            }
-        }
-
-        // Find the optimal path and show it
-        path = AStar(start, goal);
-        if (path == null)
-        {
-        	Debug.Log("Failed to find a valid path");
+            Debug.Log("Failed to find a valid path");
         }
         else
         {
-        	// Draw a line along the path
-        	Debug.Log(path[0].Position);
-	        for (int i = 0; i < path.Count - 1; i++)
-	        {
-	        	Debug.Log(path[i+1].Position);
-	        	Node u = path[i];
-	        	Node v = path[i+1];
-	        	Vector3 origin = v.Position;
-	        	Vector3 direction = u.Position - v.Position;
-	        	float dist = Vector3.Distance(u.Position, v.Position);
-	        	Debug.DrawRay(origin, direction.normalized * dist, Color.red, Mathf.Infinity);
-	        }
+            // Draw a line along the path
+            for (int i = 0; i < path.Count - 1; i++)
+            {
+                Vector3 origin = path[i];
+                Vector3 direction = path[i+1] - path[i];
+                float dist = Vector3.Distance(path[i], path[i+1]);
+                Debug.DrawRay(origin, direction.normalized * dist, Color.green, Mathf.Infinity);
+            }
 
-	        // Remove the first node from the path (which is itself)
-	        // Then the first node in the path will be the next waypoint
-	        path.RemoveAt(0);  // TODO: this is expensive, use a better datastructure
+            // Remove the first node from the path (which is itself)
+            // Then the first node in the path will be the next waypoint
+            path.RemoveAt(0);  // TODO: this is expensive, use a better datastructure
         }
     }
 
@@ -207,24 +55,29 @@ public class TargetBehaviour : MonoBehaviour
     // FixedUpdate is called once per physics update
     void FixedUpdate()
     {
-    	// Don't try to move if there is no path
-    	if (path == null || path.Count == 0) return;
+        // Only move when drone is flying
+        if (!flying) return;
 
-    	// Only move when drone is flying
-    	if (!flying) return;
+        // Don't try to move if there is no path
+        if (path == null || path.Count == 0) return;
 
-    	// If we have reached the current waypoint, select the next node in the path
-    	float distToWaypoint = Vector3.Distance(transform.position, path[0].Position);  // FIXME: after removing once, the next waypoint stays the same, so it removes twice
-    	Debug.Log(distToWaypoint);
-    	if (distToWaypoint <= waypointMargin)
-    	{
-    		Debug.Log("removing");
-    		path.RemoveAt(0);  // TODO: this is expensive, use a better datastructure
-    		Debug.Log("next waypoint: " + path[0].Position.ToString());
-    	}
+        // If we have reached the current waypoint, select the next node in the path
+        float distToWaypoint = Vector3.Distance(transform.position, path[0]);  // FIXME: after removing once, the next waypoint stays the same, so it removes twice
+        if (distToWaypoint <= waypointMargin)
+        {
+            Debug.Log("Reached waypoint");
+            path.RemoveAt(0);  // TODO: this is expensive, use a better datastructure
+        }
 
-    	// Move towards the next waypoint
-    	Vector3 direction = path[0].Position - transform.position;
-    	rb.velocity = direction.normalized * speed;
+        // Move towards the next waypoint
+        if (path.Count > 0)
+        {
+            Vector3 direction = path[0] - transform.position;
+            rb.velocity = direction.normalized * speed;
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
 }
---
+++ Assets/Utility/Pathfinding.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    // The position of the node in 3D space
+    public Vector3 position;
+
+    // All nodes that can be reached from this one
+    public List<Node> neighbors;
+
+    // The previous node in the path
+    public Node cameFrom = null;
+
+    // The cost of getting from the start to this node
+    public float gScore = Mathf.Infinity;
+
+    // The cost of getting from the start to the goal while passing by this node
+    // This value is partly known, partly heuristic
+    public float fScore = Mathf.Infinity;
+
+    public Node(Vector3 pos)
+    {
+        position = pos;
+        neighbors = new List<Node>();
+    }
+
+    /**
+    Finds the distance between two nodes.
+    */
+    public static float Distance(Node v1, Node v2)
+    {
+        return Vector3.Distance(v1.position, v2.position);
+    }
+}
+
+public static class Pathfinding
+{
+    /**
+    Traces back the path from the starting node to the current node when A* is finished.
+    */
+    static List<Node> ReconstructPath(Node current)
+    {
+        List<Node> path = new List<Node>();
+        path.Add(current);
+        while (current.cameFrom != null)
+        {
+            current = current.cameFrom;
+            path.Add(current);
+        }
+        path.Reverse();
+        return path;
+    }
+
+    /**
+    Finds the shortest path between two nodes in a graph defined a set of neighbor nodes for each node.
+    */
+    static List<Node> AStar(Node start, Node goal)
+    {
+        // The set of nodes already evaluated
+        List<Node> closedSet = new List<Node>();  // TODO: use HashSet
+
+        // The set of currently discovered nodes that are not evaluated yet
+        List<Node> openSet = new List<Node>();
+        openSet.Add(start);
+
+        // The cost of going from start to start is zero
+        start.gScore = 0;
+
+        // For the first node, the cost to get to the goal is estimated using the heuristic
+        start.fScore = Node.Distance(start, goal);
+
+        while (openSet.Count > 0)
+        {
+            // Find the node with lowest fScore
+            Node current = null;
+            float maxF = Mathf.Infinity;
+            foreach (Node v in openSet)
+            {
+                if (current == null || v.fScore < maxF)
+                {
+                    current = v;
+                    maxF = v.fScore;
+                }
+            }
+
+            if (current == goal)
+            {
+                return ReconstructPath(current);
+            }
+
+            openSet.Remove(current);
+            closedSet.Add(current);
+
+            foreach (Node neighbor in current.neighbors)
+            {
+                if (closedSet.Contains(neighbor)) continue;  // ignore neighbors that have already been evaluated
+
+                float tentativeGScore = current.gScore + Node.Distance(current, neighbor); // the distance from start to a neighbor
+
+                if (!openSet.Contains(neighbor))  // discover a new node
+                {
+                    openSet.Add(neighbor);
+                }
+                else if (tentativeGScore >= neighbor.gScore)
+                {
+                    continue;
+                }
+
+                // This path is the best yet, record it!
+                neighbor.cameFrom = current;
+                neighbor.gScore = tentativeGScore;
+                neighbor.fScore = neighbor.gScore = Node.Distance(neighbor, goal);
+            }
+        }
+
+        // We failed to find a path from start to goal
+        return null;
+    }
+
+    /**
+    Finds out what nodes have line of sight to eachother, and sets their neighbor variables accordingly.
+    */
+    static void SetNeighbors(List<Node> nodes)
+    {
+        // For each node, find its neighbors
+        // Two nodes are neighbors iff they have line of sight to eachother
+        foreach (Node v in nodes)
+        {
+            foreach (Node u in nodes)
+            {
+                if (v == u) continue;
+                Vector3 origin = v.position;
+                Vector3 direction = u.position - v.position;
+                float dist = Node.Distance(v, u);
+                int layerMask = ~(1 << 9);  // we can see through objects in Debug layer (9)
+                if (!Physics.Raycast(origin, direction, dist, layerMask))
+                {
+                    // Nodes have line of sight to eachother
+                    v.neighbors.Add(u);
+                    u.neighbors.Add(v);
+                    Debug.DrawRay(origin, direction.normalized * dist, Color.yellow, Mathf.Infinity);
+                }
+            }
+        }
+    }
+
+    /**
+    Finds the shortest path between two gameobjects that is free of collisions, using NavNodes as waypoints.
+    */
+    public static List<Vector3> GetShortestPath(GameObject startObj, GameObject goalObj)
+    {
+        return GetShortestPath(startObj.transform.position, goalObj.transform.position);
+    }
+
+    /**
+    Finds the shortest path between two points that is free of collisions, using NavNodes as waypoints.
+    */
+    public static List<Vector3> GetShortestPath(Vector3 start, Vector3 goal)
+    {
+        // Create list that will contain all nodes in the pathfinding graph
+        List<Node> nodes = new List<Node>();
+        
+        // Create nodes for NavNodes
+        GameObject[] nodeObjs = GameObject.FindGameObjectsWithTag("NavNode");
+        foreach (GameObject obj in nodeObjs)
+        {
+            nodes.Add(new Node(obj.transform.position));
+        }
+
+        // Create nodes for start and goal
+        Node startNode = new Node(start);
+        Node goalNode = new Node(goal);
+        nodes.Add(startNode);
+        nodes.Add(goalNode);
+
+        // Determine what nodes are neighbors based on line of sight
+        SetNeighbors(nodes);
+
+        // Find the optimal path
+        List<Node> path = AStar(startNode, goalNode);
+
+        // Return the path
+        if (path == null)
+        {
+            return null;
+        }
+        else
+        {
+            // Return points in path
+            List<Vector3> pathPoints = new List<Vector3>();
+            foreach (Node v in path)
+            {
+                pathPoints.Add(v.position);
+            }
+            return pathPoints;
+        }
+    }
+}
