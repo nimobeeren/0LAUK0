438f84950418e98f72e05ca78e8cba5ed48c806a
Nimo Beeren <nimobeeren@gmail.com>
2019-04-03 12:49:49 +0200

    Implement user collision

--- Assets/Behaviours/UserBehaviour.cs
+++ Assets/Behaviours/UserBehaviour.cs
@@ -7,24 +7,27 @@ public class UserBehaviour : MonoBehaviour
 	public float speed = 2;
 	public float rotationSpeed = 100;
 
-	private Rigidbody rb;
+	private CharacterController controller;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-    	// Move forwards/backwards and strafe
-        rb.velocity = speed * Input.GetAxis("Vertical") * transform.forward;
-        rb.velocity += speed * Input.GetAxis("Horizontal") * transform.right;
-
         // Rotate the user with Q and E
         float rotation = 0;
         if (Input.GetKey(KeyCode.Q)) rotation = -1;
         else if (Input.GetKey(KeyCode.E)) rotation = 1;
         transform.Rotate(new Vector3(0, rotation, 0) * Time.deltaTime * rotationSpeed, Space.World);
+
+        // Move forward/backward/left/right
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        float forwardSpeed = speed * Input.GetAxis("Vertical");
+        float sidewaysSpeed = speed * Input.GetAxis("Horizontal");
+        controller.SimpleMove(Vector3.ClampMagnitude(forward * forwardSpeed + right * sidewaysSpeed, speed));
     }
 }
