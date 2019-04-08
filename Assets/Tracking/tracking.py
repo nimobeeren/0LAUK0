aab831da8cc5f56b644d2d51b5386dbfc12da55d
Nimo Beeren <nimobeeren@gmail.com>
2019-04-08 16:25:31 +0200

    Print tracked bounding box to stdout and improve support for multiple trackers

--- Assets/Tracking/tracking.py
+++ Assets/Tracking/tracking.py
@@ -8,45 +8,74 @@ Performs object tracking on a continuously updating image using OpenCV.
 Tested using Python 3.7.2 and OpenCV 3.4.4
 """
 
-import time
 import cv2
+import os
+import time
 import sys
 
 
-def draw_bbox(list_of_bbox):
-	""" Draws BBOX on screen."""
-	if len(list_of_bbox) == 0:
+colors = {
+	'boosting': (255, 0, 0),
+	'MIL': (0, 255, 0),
+	'KCF': (0, 0, 255),
+	'TLD': (255, 255, 0),
+	'MedianFlow': (255, 0, 255),
+	'MOSSE': (0, 255, 255),
+	'CSRT': (255, 255, 255)
+}
+
+def draw_bbox(bboxes):
+	"""Draws BBOX on screen."""
+	if len(bboxes) == 0:
 		return
 	else:
-		counter = 0 
-		# MOSSE, KCF, CSRT
-		# BLUE, LIGHTBLUE, GREEN
-		cols = [(255, 0, 0), (255, 255, 0), (100, 100, 0)]
-		for bbox in list_of_bbox:
+		print_bboxes(bboxes)
+		for tracker in bboxes:
+			bbox = bboxes[tracker]
 			p1 = (int(bbox[0]), int(bbox[1]))
 			p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-			cv2.rectangle(frame, p1, p2, cols[counter], 2, 1)
-			counter = counter + 1
-		return
+			cv2.rectangle(frame, p1, p2, colors[tracker], 2, 1)
+
+
+def print_bboxes(bboxes):
+	"""Prints a list of bboxes to stdout."""
+	counter = 0
+	for b in bboxes:
+		if counter > 0:
+			break  # only print the first bbox
+
+		if len(bboxes[b]) < 4:
+			raise ValueError("Bounding box has invalid format")
+		print(bboxes[b])
+		counter += 1
+
+
+def read_image(file):
+	frame = cv2.imread(file)
+
+	# For some reason, when our drone camera outputs in PPM format, the image is flipped horizontally,
+	# so we flip it back
+	filename, extension = os.path.splitext(file)
+	if extension.lower() == ".ppm":
+		frame = cv2.flip(frame, 0)  # flip image horizontally
+
+	return frame
 
 
 if __name__ == '__main__':
-	# Print python and opencv version
-	print("Using Python version: ", sys.version.split('|')[0])
-	print("Using OpenCV version: ", cv2.__version__)
-
-	# Path to image file
+	# Get path to image file
 	if len(sys.argv) > 1:
-		file = sys.argv[1]
+		file = os.path.abspath(sys.argv[1])
 	else:
-		file = "../../scrot.jpg"
+		file = os.path.abspath("droneCam.ppm")
+
+	print(file)
 
 	# Read first frame from image
-	frame = cv2.imread(file)
+	frame = read_image(file)
 
 	# Let user select a bounding box using the first frame
 	bbox = cv2.selectROI(frame, False)
-	print("Selected bbox: ", bbox)
 
 	# Create trackers
 	# Running all trackers at the same time results in 2 FPS, so a selection has been made
@@ -55,9 +84,9 @@ if __name__ == '__main__':
 	# trackers['MIL'] = cv2.TrackerMIL_create()
 	# trackers['KCF'] = cv2.TrackerKCF_create()
 	# trackers['TLD'] = cv2.TrackerTLD_create()
-	trackers['MedianFlow'] = cv2.TrackerMedianFlow_create()
-	trackers['MOSSE'] = cv2.TrackerMOSSE_create()
-	# trackers['CSRT'] = cv2.TrackerCSRT_create()
+	# trackers['MedianFlow'] = cv2.TrackerMedianFlow_create()
+	# trackers['MOSSE'] = cv2.TrackerMOSSE_create()
+	trackers['CSRT'] = cv2.TrackerCSRT_create()
 
 	# Initialize trackers using specified bounding box
 	for t in trackers:
@@ -77,7 +106,7 @@ if __name__ == '__main__':
 
 		# Try to read image
 		try:
-			frame = cv2.imread(file)
+			frame = read_image(file)
 			if frame is None:
 				raise Exception('Failed to read image')
 		except Exception as e:
@@ -89,13 +118,11 @@ if __name__ == '__main__':
 		fails = 0
 
 		# Run trackers
-		bboxes = []
+		bboxes = {}
 		for t in trackers:
 			success, bbox = trackers[t].update(frame)
-			print(t + ': ' + str(success) + ' | ', end='')
 			if success:
-				bboxes.append(bbox)
-		print('\r', end='')
+				bboxes[t] = bbox
 
 		# Draw resulting bounding boxes on screen
 		draw_bbox(bboxes)
@@ -106,8 +133,10 @@ if __name__ == '__main__':
 		cv2.putText(frame, "FPS : " + str(int(fps)), (400,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
 
 		# Draw names with respective colors
-		cv2.putText(frame, "MOSSE ", (0,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 0, 0),2);
-		cv2.putText(frame, "MedianFlow ", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 0),2);
+		y = 20
+		for t in trackers:
+			cv2.putText(frame, t, (0, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, colors[t], 2);
+			y += 20
 		
 		# Display result
 		cv2.imshow("Tracking", frame)
