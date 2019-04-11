b80a090d32240277280eeed238bc3ee0c33ce6d4
Nimo Beeren <nimobeeren@gmail.com>
2019-04-11 17:33:17 +0200

    Attempt to use tracking results for target positioning (disabled by default for now)

--- Assets/Behaviours/TargetBehaviour.cs
+++ Assets/Behaviours/TargetBehaviour.cs
@@ -12,6 +12,7 @@ public class TargetBehaviour : MonoBehaviour
     public float toleranceAngle = 45;    // maximum angle at which the user can be to the drone before it adjusts sideways
     public float defaultDistance = 4;    // distance to the user that the drone should try to keep
     public float stabilizationTime = 1;  // currently ununsed
+    public bool useTracking = false;     // whether to use tracking to determinine user position
 
     /* Pathfinding variables */
     private List<Vector3> path;          // sequence of waypoints to follow to get to the destination
@@ -19,7 +20,7 @@ public class TargetBehaviour : MonoBehaviour
 
     /* Tolerance zone variables */
     private Mesh zoneMesh;               // mesh of tolerance zone visualization
-    private Vector3 userDir0;      // last significant direction of the user from the drone
+    private Vector3 userPos0;      // last significant direction of the user from the drone
 
     /* Tracking variables */
     private Tracking tracking;           // used to get a bounding box of the user
@@ -34,7 +35,7 @@ public class TargetBehaviour : MonoBehaviour
     {
         zoneMesh.Clear();
 
-        Vector3 U = userDir0.normalized;
+        Vector3 U = userPos0.normalized;
         Vector3 UT = new Vector3(U.z, 0, -U.x);  // orthogonal to U
 
         zoneMesh.vertices = new Vector3[] {
@@ -50,51 +51,63 @@ public class TargetBehaviour : MonoBehaviour
     // Gets the relative movement 
     Vector3 GetMovement()
     {
-        // Get user-target direction and distance
-        Vector3 realUserDir = user.transform.position - transform.position;
-        realUserDir.y = 0;  // project onto the horizontal plane
-        float realUserAngle = Vector3.Angle(realUserDir, userDir0);
-        float realUserDist = realUserDir.magnitude;
+        // Get position of user relative to target
+        Vector3 userPos;
+        if (useTracking && tracking != null)
+        {
+            // Get position using bounding box produced by tracking algorithm
+            Vector3 droneToUser = tracking.GetObjectPosition(droneCam, userHeight);
+            if (droneToUser.magnitude == 0)
+            {
+                // We don't know where the user is, so don't move
+                return Vector3.zero;
+            }
 
-        // Estimate user position using tracking
-        Vector3 estUserPosition = drone.transform.position + tracking.GetObjectPosition(droneCam, userHeight);
-        Debug.DrawLine(drone.transform.position, estUserPosition, Color.red);
+            userPos = drone.transform.position + droneToUser;
 
-        // Get drone direction and distance
-        Vector3 droneDir = drone.transform.position - transform.position;
-        droneDir.y = 0;  // project onto the horizontal plane
-        float droneDist = droneDir.magnitude;
+            // Draw a line for debugging
+            Debug.DrawLine(drone.transform.position, userPos, Color.red);
+        }
+        else
+        {
+            // Get position by accessing the GameObject's transform (cheating)
+            userPos = user.transform.position - transform.position;
+            userPos.y = 0;  // project onto the horizontal plane
+        }
 
-        // Get distance and direction of next waypoint
-        Vector3 nextWaypointDir = path[nextWaypoint] - transform.position;
-        float nextWaypointDist = nextWaypointDir.magnitude;
+        // Get distance and angle of user to target
+        float userDist = userPos.magnitude;
+        float userAngle = Vector3.Angle(userPos, userPos0);
+
+        // Get position and distance of next waypoint
+        Vector3 nextWaypointPos = path[nextWaypoint] - transform.position;
+        float nextWaypointDist = nextWaypointPos.magnitude;
 
         // Output debug information
         if (gui)
         {
-            gui.SetVar("Real user-target angle", realUserAngle);
-            gui.SetVar("Real user-target distance", realUserDist);
-            gui.SetVar("Estimated user distance", estUserPosition.magnitude);
-            gui.SetTargetInfo(transform.position, realUserDir, userDir0);
+            gui.SetVar("User-target angle", userAngle);
+            gui.SetVar("User-target distance", userDist);
+            gui.SetTargetInfo(transform.position, userPos, userPos0);
         }
 
         // Determine relative target movement
         if (nextWaypointDist > 0 &&
-            realUserDist < 3f/4f * defaultDistance / Mathf.Cos(Mathf.Deg2Rad * realUserAngle) * stabilizationTime)
+            userDist < 3f/4f * defaultDistance / Mathf.Cos(Mathf.Deg2Rad * userAngle) * stabilizationTime)
         {
             Debug.Log("User too close");
-            float moveDist = Mathf.Min(defaultDistance - realUserDist, nextWaypointDist);  // make sure we don't move past the waypoint
-            return moveDist * nextWaypointDir.normalized;
+            float moveDist = Mathf.Min(defaultDistance - userDist, nextWaypointDist);  // make sure we don't move past the waypoint
+            return moveDist * nextWaypointPos.normalized;
         }
-        if (realUserAngle > toleranceAngle / 2f * stabilizationTime)
+        if (userAngle > toleranceAngle / 2f * stabilizationTime)
         {
             Debug.Log("User went sideways");
 
-            Vector3 waypointUserDir = user.transform.position - path[nextWaypoint];
-            float beta = Mathf.Deg2Rad * Vector3.SignedAngle(realUserDir, waypointUserDir, Vector3.up);
+            Vector3 waypointToUserDir = user.transform.position - path[nextWaypoint];
+            float beta = Mathf.Deg2Rad * Vector3.SignedAngle(userPos, waypointToUserDir, Vector3.up);
             float delta = Mathf.PI/2 - beta;
-            float moveDist = realUserDist * Mathf.Sin(beta);
-            Vector3 move = moveDist * realUserDir.normalized;  // vector with correct magnitude but in direction of user
+            float moveDist = userDist * Mathf.Sin(beta);
+            Vector3 move = moveDist * userPos.normalized;  // vector with correct magnitude but in direction of user
 
             // Rotate move vector in correct direction (perpendicular to line between user and next waypoint)
             move = new Vector3(
@@ -103,16 +116,19 @@ public class TargetBehaviour : MonoBehaviour
                 move.x * Mathf.Sin(delta) + move.z * Mathf.Cos(delta)
             );
 
-            // Set new significant user direction
-            userDir0 = user.transform.position + move - transform.position;
-            userDir0.y = 0;  // project onto horizontal plane
+            // Set new significant user position
+            userPos0 = user.transform.position + move - transform.position;
+            userPos0.y = 0;  // project onto horizontal plane
+
+            // Point drone camera towards user
+            droneCam.transform.forward = userPos0;
             
             return move;
         }
-        if (realUserDist > 3f/2f * defaultDistance / Mathf.Cos(Mathf.Deg2Rad * realUserAngle) * stabilizationTime)
+        if (userDist > 3f/2f * defaultDistance / Mathf.Cos(Mathf.Deg2Rad * userAngle) * stabilizationTime)
         {
             Debug.Log("User too far");
-            return (realUserDist - defaultDistance) * realUserDir.normalized;
+            return (userDist - defaultDistance) * userPos.normalized;
         }
 
         return Vector3.zero;
@@ -142,14 +158,17 @@ public class TargetBehaviour : MonoBehaviour
         }
 
         // Set initial tolerance zone position
-        userDir0 = user.transform.position - transform.position;
-        userDir0.y = 0;  // project onto the horizontal plane
+        userPos0 = user.transform.position - transform.position;
+        userPos0.y = 0;  // project onto the horizontal plane
         UpdateToleranceZone();
 
         // Initialize tracking
-        tracking = new Tracking();
-        droneCam = GameObject.Find("DroneCamera");
-        userHeight = user.GetComponent<Renderer>().bounds.size.y;
+        if (useTracking)
+        {
+            tracking = new Tracking();
+            droneCam = GameObject.Find("DroneCamera");
+            userHeight = user.GetComponent<Renderer>().bounds.size.y;
+        }
     }
 
     // FixedUpdate is called once per physics update
@@ -178,6 +197,9 @@ public class TargetBehaviour : MonoBehaviour
     // Called when the object is destroyed or the game exits
     void OnDestroy()
     {
-        tracking.Stop();
+        if (tracking != null)
+        {
+            tracking.Stop();
+        }
     }
 }
